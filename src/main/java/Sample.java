611c95a2124e81b77957af1f05395e1ee9e9f52f
asuender <asuender@student.tgm.ac.at>
2022-11-29 10:24:51 +0100

    Initial commit

---
+++ src/main/java/Sample.java
@@ -0,0 +1,9 @@
+/**
+ * @author Andreas Suender
+ * @version 11-29-2022
+ */
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
